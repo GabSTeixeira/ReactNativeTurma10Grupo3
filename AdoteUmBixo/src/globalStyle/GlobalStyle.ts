b152fb980a1b134df730d0da53af614d83de26a0
Gabriel <gabrielteixeira.21@hotmail.com>
2023-11-15 21:25:31 -0300

    primeiro commit

---
+++ AdoteUmBixo/src/globalStyle/GlobalStyle.ts
@@ -0,0 +1,44 @@
+
+import { StyleSheet } from "react-native";
+
+const GlobalStyle = StyleSheet.create({
+
+    fullBranco: {
+        color: '#ffffff'
+    },
+
+    fullPreto: {
+        color: '#2f2f2f'
+    },
+
+    azul: {
+        color: '#242E47'
+    },
+
+    rosa: {
+        color: '#D16B78'
+    },
+
+    opala: {
+        color: '#A8C4B8'
+    },
+
+    branco: {
+        color: '#EDEDEF'
+    },
+
+    verde: {
+        color: '#1BBCB6'
+    },
+
+    titulo: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+
+    texto: {
+        fontSize: 14
+    }
+})
+
+export default GlobalStyle
