b1875a0523aab570bac6698b7e03260e9c4f327d
Gabriel <gabrielteixeira.21@hotmail.com>
2023-11-17 03:56:37 -0300

    automatização do acesso ao token, a cada uma hora um novo token é emitido automaticamento

--- AdoteUmBixo/App.tsx
+++ AdoteUmBixo/App.tsx
@@ -1,14 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useEffect, useState } from 'react';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { carregarAnimais } from './src/services/api/axios/AnimalApi';
+import { AnimalApiResponse } from './src/services/api/axios/Types';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function App() {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [animais, setAnimais] = useState<AnimalApiResponse[]>([])
+
+  useEffect (() => {
+
+    carregarAnimais()
+    .then(res => setAnimais((res)? res.animals: []))
+    .then(()=>setLoading(false))
+    .catch(()=> setLoading(true))
+    
+    
+  }, [])
+
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>inicio</Text>
+    <SafeAreaView>
+
+   
+      {(loading) ? <Text>"loading..."</Text>: (
+
+        
+        <FlatList 
+        data={animais}
+        keyExtractor={item => ''+item.id}
+        renderItem={({item}) => (
+          <Image width={500} height={500} source={{uri:(item?.photos?.[0]?.full)}}/>
+          
+          )}
+          />
+          )
+        }
+        
+      
       <StatusBar style="auto" />
-    </View>
+   
+  </SafeAreaView>
   );
 }
 
