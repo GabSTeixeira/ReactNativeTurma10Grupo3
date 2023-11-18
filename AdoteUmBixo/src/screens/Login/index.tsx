dd6db119b1f45cc6544287f416c897824355e6da
brayangazale <142418825+brayangazale@users.noreply.github.com>
2023-11-18 13:09:12 -0300

    faltaImgFonte

--- AdoteUmBixo/src/screens/Login/index.tsx
+++ AdoteUmBixo/src/screens/Login/index.tsx
@@ -1,12 +1,46 @@
-import { SafeAreaView } from "react-native-safe-area-context"
-import { Text } from 'react-native'
+// Importações necessárias
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles'; // Importa estilos
 
-const Login = () => {
-    return (
-        <SafeAreaView>
-            <Text> Login</Text>
-        </SafeAreaView>
-    )
-}
+const LoginScreen: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-export default Login
+  const handleLogin = () => {
+    // Lógica de autenticação aqui
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+       <Text style={styles.label}>Login</Text>
+      
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Digite seu email"
+        keyboardType="email-address"
+      />
+
+      <Text style={styles.label}>Senha</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Digite sua senha"
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.label}>Não tem cadastro? Cadastre-se aqui</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
---
+++ AdoteUmBixo/src/screens/Login/styles.ts
@@ -0,0 +1,38 @@
+// Importações necessárias
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  input: {
+    width: '70%',
+    borderRadius: 30,
+    height: 40,
+    borderColor: '#D9D9D9',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+  loginButton: {
+    backgroundColor: '#1bbcb6',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 20,
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
+
+export default styles;
